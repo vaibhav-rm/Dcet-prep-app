e6ffb064ad9ddb56be1ade1757dde896aad19210
Sagar-rm <sagarrathod292007@gmail.com>
2024-12-21 12:10:56 +0530

    questions added

--- questions.py
+++ questions.py
@@ -70,41 +70,90 @@ questions_list = [
         "What refers to a detailed description of the expected outcome of a project?",
         ["Project scope", "Project operation", "Project objective", "Project process"],
         "Project objective"
+    ),
+    Question(
+        "In flowcharts, circles are used as connector symbols to show the continuation of the flow across multiple pages or through a complex chart.",
+        ["Rectangle", "Diamond", "Oval", "Circle"],
+        "Circle"
+    ),
+    Question(
+        "Which symbol is used to represent start or stop in a flowchart?",
+        ["Rectangle", "Diamond", "Oval", "Circle"],
+        "Oval"
+    ),
+    Question(
+        "What does CSS stand for?",
+        ["Colourful Style Sheet", "Cascading Style Sheet", "Creative Style Sheet", "Computer Style Sheet"],
+        "Cascading Style Sheet"
+    ),
+    Question(
+        "Which of the following is not a web browser?",
+        ["Google Chrome", "Netscape Navigator", "Safari", "Apache"],
+        "Apache"
+    ),
+    Question(
+        "What is the correct syntax to change the background colour with CSS?",
+        ["background-color = 'purple';", "background – color : purple;", "background – color = orange", "change-background-color : purple"],
+        "background – color : purple;"
+    ),
+    Question(
+        "Which HTML tag gives a scrolling effect to a given text?",
+        ["<div>", "<scroll>", "<marquee>", "<effect>"],
+        "<marquee>"
+    ),
+    Question(
+        "ERP package will handle ______ business functionality / functionalities.",
+        ["One", "Two", "Three", "Multiple"],
+        "Multiple"
+    ),
+    Question(
+        "Which one is a type of organizational structure?",
+        ["Responsibility structure", "Behavioural structure", "Functional structure", "Relational structure"],
+        "Functional structure"
+    ),
+    Question(
+        "______ describes how an actor uses a system to accomplish a particular goal for describing the activities.",
+        ["Process case", "Work case", "Use case", "Sub process"],
+        "Use case"
+    ),
+    Question(
+        "Which of the following is not a characteristic of First-generation ERP system?",
+        ["They were based on mainframe computer", "They focused on accounting and finance", "They used centralized database", "They supported real-time data processing"],
+        "They supported real-time data processing"
+    ),
+    Question(
+        "Which cloud service model enables users to develop, run, manage applications without worrying about underlying infrastructure?",
+        ["IAAS", "PAAS", "SAAS", "NAAS"],
+        "PAAS"
+    ),
+    Question(
+        "Which of the following is not a Google application?",
+        ["Google Docs", "Google Slides", "Google Sheets", "Google Word"],
+        "Google Word"
+    ),
+    Question(
+        "IoT is a ______.",
+        ["Network of sensors", "Network of objects in a ring structure", "Network of virtual objects", "Network of physical objects embedded with sensors"],
+        "Network of physical objects embedded with sensors"
+    ),
+    Question(
+        "______ is a file storage and synchronization service developed by Google which provides complete office tools with cloud storage.",
+        ["Google Colab", "Google Drive", "Google Meet", "AWS Cloud"],
+        "Google Drive"
+    ),
+    Question(
+        "Which of the following is not a security issue in a personal computer?",
+        ["Ransomware Attack", "Data Breach", "Code Injection", "IoT"],
+        "IoT"
+    ),
+    Question(
+        "Which of the following is not a firewall?",
+        ["Packet filtering", "Stateful inspection", "Panda", "Circuit Level Gateway"],
+        "Panda"
+    ),
+    Question(
+        "'S' in HTTPS stands for ______.",
+        ["Safe", "Secure", "System", "Server"],
+        "Secure"
     )
-    Question("In flowcharts, circles are used as connector symbols to show the continuation of the flow across multiple pages or through a complex chart.", 
-    ["Rectangle", "Diamond", "Oval", "Circle"], 
-    "Circle");
-
-Question("Which symbol is used to represent start or stop in a flowchart?", 
-    ["Rectangle", "Diamond", "Oval", "Circle"], 
-    "Oval");
-
-Question("What does CSS stand for?", 
-    ["Colourful Style Sheet", "Cascading Style Sheet", "Creative Style Sheet", "Computer Style Sheet"], 
-    "Cascading Style Sheet");
-
-Question("Which of the following is not a web browser?", 
-    ["Google Chrome", "Netscape Navigator", "Safari", "Apache"], 
-    "Apache");
-
-Question("What is the correct syntax to change the background colour with CSS?", 
-    ["background-color = 'purple';", "background – color : purple;", "background – color = orange", "change-background-color : purple"], 
-    "background – color : purple;");
-
-Question("Which HTML tag gives a scrolling effect to a given text?", 
-    ["<div>", "<scroll>", "<marquee>", "<effect>"], 
-    "<marquee>");
-
-Question("ERP package will handle ______ business functionality / functionalities.", 
-    ["One", "Two", "Three", "Multiple"], 
-    "Multiple");
-
-Question("Which one is a type of organizational structure?", 
-    ["Responsibility structure", "Behavioural structure", "Functional structure", "Relational structure"], 
-    "Functional structure");
-
-Question("______ describes how an actor uses a system to accomplish a particular goal for describing the activities.", 
-    ["Process case", "Work case", "Use case", "Sub process"], 
-    "Use case");
-
 ]
