03313eb8f2206e905ab31d741e251a53406be9f9
vaibhav-rm <rathodvaibhav401@gmail.com>
2025-02-13 23:13:26 +0530

    deleted duplicated test

--- questions.py
+++ questions.py
@@ -269,17 +269,5 @@ questions_list = [
         ["A temporary endeavor with a defined beginnning and end", "An ongoing activity with no specific end date","electro-magnetic","electro-dynamic"],
         "A temporary endeavor with a defined beginnning and end",
         "Project Management Skills"
-    ),
-            Question(
-        "",
-        ["A temporary endeavor with a defined beginnning and end", "An ongoing activity with no specific end date","electro-magnetic","electro-dynamic"],
-        "A temporary endeavor with a defined beginnning and end",
-        "Mathematics"
-    ),
-                Question(
-        "",
-        ["A temporary endeavor with a defined beginnning and end", "An ongoing activity with no specific end date","electro-magnetic","electro-dynamic"],
-        "A temporary endeavor with a defined beginnning and end",
-        "Mathematics"
     )
 ]
